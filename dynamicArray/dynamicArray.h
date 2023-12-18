c92fe4181ce13398666bf729ae0df4e578ada260
choclollipop <2924733894@qq.com>
2023-12-18 11:19:25 +0800

    [修改] 新增动态数组的结点

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -1,6 +1,14 @@
 #ifndef __DYNAMIC_ARRAY_H_
 #define __DYNAMIC_ARRAY_H_
 
+typedef int ELEMENTTYPE;
+
+typedef struct dynamicArray
+{
+    ELEMENTTYPE *data;
+    int len;
+    int capacity;
+}dynamicArray;
 
 
 #endif // __DYNAMIC_ARRAY_H_
