0cc89d6ce91ecc32b64ad590ca80732b1673f4a2
choclollipop <2924733894@qq.com>
2023-12-18 15:17:30 +0800

    [修改] 完成动态数组的插入

--- dynamicArray/dynamicArry.c
+++ dynamicArray/dynamicArry.c
@@ -9,9 +9,13 @@ enum STATUS_CODE
     ON_SUCCESS,
     NULL_PTR,
     MALLOC_ERROR,
+    INVALID_ACCESS,
 };
 
 #define DEFAULT_SIZE    10
+
+/* 静态函数前置声明 */
+static int expendDynamicArray(dynamicArray * pArray);
 
 /* 初始化动态数组 */
 int dynamicArrayInit(dynamicArray * pArray, int capacity)
@@ -44,10 +48,79 @@ int dynamicArrayInit(dynamicArray * pArray, int capacity)
 }
 
 /* 动态数组插入数据(默认在末尾位置插入) */
-int dynamicArrayInsertData(dynamicArray * pArray, ELEMENTTYPE val);
+int dynamicArrayInsertData(dynamicArray * pArray, ELEMENTTYPE val)
+{
+    return dynamicArrayAppointPosInsertData(pArray, pArray->len, val);
+}
+
+/* 动态数组扩容 */
+static expendDynamicArray(dynamicArray * pArray)
+{
+    /* 自己写成了 dynamicArry * pTmp = pArry; 只需要指向数据的指针就好，不是动态数组类型的 */
+    ELEMENTTYPE * pTmp = pArray->data;
+
+    int needExpandCapacity = pArray->capacity + (pArray->capacity >> 1);
+
+    /* 将原来的容量增加至增加1.5倍 */
+    pArray->data = (dynamicArray *)malloc(sizeof(ELEMENTTYPE) * needExpandCapacity);
+    if(!pArray)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 移动数据 */
+    for(int idx = 0; idx < pArray->len; idx++)
+    {
+        pArray->data[idx] = pTmp[idx];
+    }
+
+    /* 释放之前的内存空间 */
+    if(!pTmp)
+    {
+        free(pTmp);
+        pTmp = NULL;
+    }
+
+    /* 更新数组容量 */
+    pArray->capacity = needExpandCapacity;
+    
+    return 0;
+}
 
 /* 动态数组在指定位置插入数据 */
-int dynamicArrayAppointPosInsertData(dynamicArray * pArray, int pos, ELEMENTTYPE val);
+int dynamicArrayAppointPosInsertData(dynamicArray * pArray, int pos, ELEMENTTYPE val)
+{
+    /* 指针判空 */
+    if(!pArray)
+    {
+        return NULL_PTR;
+    }
+
+    /* 判断位置的合法性 */
+    if(pos < 0 || pos > pArray->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+    /* 开始扩容:算法是数组扩容的临界值是，数组大小的1.5倍 >= 数组容量 */
+    if( pArray->len + (pArray->len >> 1) >= pArray->capacity)
+    {
+        expendDynamicArray(pArray);
+    }
+    
+
+    /* 数据后移，留出pos位置插入数据 */
+    for(int idx = pArray->len; idx > pos; idx--)
+    {
+        pArray->data[idx] = pArray->data[idx - 1];
+    }
+
+    /* 找到对应的值，写入到数组中 */
+    pArray->data[pos] = val;
+    pArray->len += 1;
+
+    return ON_SUCCESS;
+}
 
 /* 动态数组修改指定位置的数据 */
 int dynamicArrayModifyAppointPosData(dynamicArray * pArray, int pos, ELEMENTTYPE val);
