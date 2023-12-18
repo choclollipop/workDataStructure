4b28c92e531f41daa7b8b3c732ca14142aa6a798
choclollipop <2924733894@qq.com>
2023-12-18 17:29:55 +0800

    [新增] 新增测试用例

--- dynamicArray/main.c
+++ dynamicArray/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "dynamicArray.h"
+#include <string.h>
 
 #define BUFFER_SIZE    10
-#define DEFAULT_NUM    3
+#define DEFAULT_NUM    5
+
+typedef struct stuInfo
+{
+    int age;
+    char sex;
+}stuInfo;
+
 
 int main()
 {
@@ -10,22 +18,77 @@ int main()
 
     dynamicArrayInit(&array, BUFFER_SIZE);
 
-    for(int idx = 0; idx < DEFAULT_NUM; idx++)
+#if 0
     {
-        dynamicArrayInsertData(&array, idx);
+        /* 插入数据 */
+        for(int idx = 0; idx <= DEFAULT_NUM; idx++)
+        {
+            dynamicArrayInsertData(&array, idx);
+        }
     }
 
-    int size = 0;
-    dynamicArrayGetSize(&array, &size);
-    printf("size : %d\n", size);
+    {
+        int size = 0;
+        /* 获取数组大小 */
+        dynamicArrayGetSize(&array, &size);
+        printf("size : %d\n", size);
 
-    int val = 0;
+        /* 打印数据 */
+        int val = 0;
+        for(int idx = 0; idx < size; idx++)
+        {
+            dynamicArrayGetAppointVal(&array, idx, &val);
+            printf("val : %d\t", val);
+        }
+        printf("\n");
+
+    }
+
+    {
+        /* 删除末尾元素 */
+        dynamicArrayDeleteData(&array);
+
+        for(int idx = 0; idx < size; idx++)
+        {
+            dynamicArrayGetAppointVal(&array, idx, &val);
+            printf("val : %d\t", val);
+        }
+    
+        printf("\n");
+    }
+
+    /* 删除指定位置的数据 */
+    int pos = 1;
+    dynamicArrayDeleteAppointData(&array, pos);
     for(int idx = 0; idx < size; idx++)
     {
         dynamicArrayGetAppointVal(&array, idx, &val);
-        printf("val : %d\n", val);
+        printf("val : %d\t", val);
     }
-    
+    printf("\n");
+
+#endif
+
+#if 0
+    stuInfo stu1,stu2,stu3;
+    memset(&stu1, 0, sizeof(stu1));
+    memset(&stu2, 0, sizeof(stu2));
+    memset(&stu3, 0, sizeof(stu3));
+
+    stu1.age = 10;
+    stu1.sex = 'm';
+
+    stu2.age = 20;
+    stu2.sex = 'f';
+
+    stu3.age = 30;
+    stu3.sex = 'm';
+
+    dynamicArrayInsertData(&array, stu1);
+    dynamicArrayInsertData(&array, stu2);
+    dynamicArrayInsertData(&array, stu3);
+
+#endif
 
     return 0;
 }
