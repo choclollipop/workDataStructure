ae1d20428d57a3833ba5cd4ba4e571e67ab9f6f8
choclollipop <2924733894@qq.com>
2023-12-20 11:14:37 +0800

    [修改] 完成链表的插入接口 && 获取链表长度的API接口

--- linkList/LinkList.c
+++ linkList/LinkList.c
@@ -44,19 +44,58 @@ int LinkListInit(LinkList ** pList)
 /* 链表头插 */
 int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, 0, val);
 }
 
 /* 链表尾插 */
 int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, pList->len, val);
 }
 
 /* 链表指定位置插入 */
 int LinkListAppointPosInsert(LinkList * pList,int pos, ELEMENTTYPE val)
 {
+    if(!pList)
+    {
+        return NULL_PTR;
+    }
 
+    if(pos < 0 || pos > pList->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+    /* 创建新节点 */
+    LinkNode * newNode = (LinkNode *)malloc(sizeof(LinkNode) * 1);
+    if(!newNode)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(newNode, 0, sizeof(LinkNode) * 1);
+    newNode->data = 0;
+    newNode->next = NULL;
+
+    /* 赋值 */
+    newNode->data = val;
+
+    /* 从头结点开始遍历 */
+    LinkNode * travel = pList->head;
+
+    while(pos)
+    {
+        travel = travel->next;
+        pos--;
+    }
+
+    /* 执行插入操作 */
+    newNode = travel->next;
+    travel->next = newNode;
+
+    /* 更新链表数据 */
+    pList->len++;
+
+    return ON_SUCCESS;
 }
 
 /* 链表头删 */
@@ -86,7 +125,17 @@ int LinkListDelAppointVal(LinkList * pList, ELEMENTTYPE val)
 /* 获取链表长度 */
 int LinkListGetLen(LinkList * pList, int * pSize)
 {
+    if(!pList)
+    {
+        return NULL_PTR;
+    }
 
+    if(pSize)
+    {
+        *pSize = pList->len;
+    }
+
+    return ON_SUCCESS;
 }
 
 /* 销毁链表 */
