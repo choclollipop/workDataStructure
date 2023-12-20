3055bd017b47af6fc6d9907e01d4149bb14dd219
choclollipop <2924733894@qq.com>
2023-12-20 10:13:19 +0800

    [修改] 完成函数的形参

---
+++ linkList/LinkList.c
@@ -0,0 +1,63 @@
+#include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
+
+/* 链表初始化 */
+int LinkListInit(LinkList ** pList)
+{
+
+}
+
+/* 链表头插 */
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val)
+{
+
+}
+
+/* 链表尾插 */
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val)
+{
+
+}
+
+/* 链表指定位置插入 */
+int LinkListAppointPosInsert(LinkList * pList,int pos, ELEMENTTYPE val)
+{
+
+}
+
+/* 链表头删 */
+int LinkListHeadDel(LinkList * pList)
+{
+
+}
+
+/* 链表尾删 */
+int LinkListTailDel(LinkList * pList)
+{
+
+}
+
+/* 链表指定位置删除 */
+int LinkListAppointPosDel(LinkList * pList, int pos)
+{
+
+}
+
+/* 链表删除指定元素 */
+int LinkListDelAppointVal(LinkList * pList, ELEMENTTYPE val)
+{
+
+}
+
+/* 获取链表长度 */
+int LinkListGetLen(LinkList * pList, int * pSize)
+{
+
+}
+
+/* 销毁链表 */
+int LinkListDestory(LinkList * pList)
+{
+    
+}
--- linkList/LinkList.h
+++ linkList/LinkList.h
@@ -18,33 +18,33 @@ typedef struct LinkList
 }LinkList;
 
 /* 链表初始化 */
-int LinkListInit();
+int LinkListInit(LinkList ** pList);
 
 /* 链表头插 */
-int LinkListHeadInsert();
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val);
 
 /* 链表尾插 */
-int LinkListTailInsert();
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val);
 
 /* 链表指定位置插入 */
-int LinkListAppointPosInsert();
+int LinkListAppointPosInsert(LinkList * pList,int pos, ELEMENTTYPE val);
 
 /* 链表头删 */
-int LinkListHeadDel();
+int LinkListHeadDel(LinkList * pList);
 
 /* 链表尾删 */
-int LinkListTailDel();
+int LinkListTailDel(LinkList * pList);
 
 /* 链表指定位置删除 */
-int LinkListAppointPosDel();
+int LinkListAppointPosDel(LinkList * pList, int pos);
 
 /* 链表删除指定元素 */
-int LinkListDelAppointVal();
+int LinkListDelAppointVal(LinkList * pList, ELEMENTTYPE val);
 
 /* 获取链表长度 */
-int LinkListGetLen();
+int LinkListGetLen(LinkList * pList, int * pSize);
 
 /* 销毁链表 */
-int LinkListDestory();
+int LinkListDestory(LinkList * pList);
 
 #endif //__LINK_LIST_H_
