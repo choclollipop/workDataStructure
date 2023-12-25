c8e1b7e1f5860b2c39eaa1a8e0ee7260777eed50
choclollipop <2924733894@qq.com>
2023-12-25 15:23:26 +0800

    [修改] 完成二叉搜索树的初始化API接口

--- binarySearchTree/binarySearchTree.c
+++ binarySearchTree/binarySearchTree.c
@@ -1,9 +1,50 @@
 #include "binarySearchTree.h"
+#include <stdlib.h>
+#include <string.h>
+
+/* 状态码 */
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+    NOT_FIND,
+};
 
 /* 二叉搜索树的初始化 */
 int binarySearchTreeInit(binarySearchTree ** pBstree)
 {
-    return 0;
+    /* 先给二叉树分配空间 */
+    binarySearchTree * bstree = (binarySearchTree *)malloc(sizeof(binarySearchTree) * 1);
+    if(!bstree)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 给分配的空间初始化 */
+    memset(bstree, 0, sizeof(binarySearchTree) * 1);
+
+    /* 给树初始化值 */
+    bstree->root = NULL;
+    bstree->size = 0;
+
+    /* 给根节点分配空间 */
+    bstree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if(!bstree->root)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(bstree->root, 0, sizeof(BSTreeNode) * 1);
+
+    /* 清除根节点数据 */
+    bstree->root->data = 0;
+    bstree->root->left = NULL;
+    bstree->root->right = NULL;
+    bstree->root->parent = NULL;
+
+    *pBstree = bstree;
+
+    return ON_SUCCESS;
 }
 
 /* 二叉搜索树的插入 */
