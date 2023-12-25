262abd97ed99a625e1ec87b669f4b12ed09cf0ac
choclollipop <2924733894@qq.com>
2023-12-25 15:50:06 +0800

    [新增] 完善二叉搜索树的插入逻辑结构

--- binarySearchTree/binarySearchTree.c
+++ binarySearchTree/binarySearchTree.c
@@ -50,5 +50,61 @@ int binarySearchTreeInit(binarySearchTree ** pBstree)
 /* 二叉搜索树的插入 */
 int binarySearchTreeInsert(binarySearchTree * pBstree, ELELMENTTYPE val)
 {
+    if(!pBstree)
+    {
+        return NULL_PTR;
+    }
+
+    if(pBstree->size == 0)
+    {
+        /* 更新树的结点 */
+        pBstree->size++;
+        pBstree->root->data = val;
+        
+        return ON_SUCCESS;
+    }
+    
+    /* 从根节点开始遍历 */
+    BSTreeNode * travelNode = pBstree->root;
+
+    /* 指向父节点的父指针 */
+    BSTreeNode * parentNode = pBstree->root;
+
+    /* 标记传入的参数 */
+    int cmp = 0;
+
+    while(travelNode != NULL)
+    {
+        /* 标记父节点 */
+        parentNode = travelNode;
+
+        cmp = val - travelNode->data;
+
+        /* 比较插入的结点与各个结点的大小关系 */
+        /* tmp小于0，则放在左子树；大于0，则放在右子树 */
+        if(cmp < 0)
+        {
+            travelNode = travelNode->left;
+        }
+        else if(cmp > 0)
+        {
+            travelNode = travelNode->right;
+        }
+        else
+        {
+            /* 如果插入结点与遍历到的结点相同，直接返回 */
+            return ON_SUCCESS;
+        }
+    }
+
+    if(cmp < 0)
+    {
+        parentNode->left = (val的结点);
+    }
+    else
+    {
+        parentNode->right = (val的结点);
+    }
+
     return 0;
 }
